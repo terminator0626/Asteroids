a3a0132c9a595329206c83b19ec9a6f7b3f92e56
terminator0626 <terminator.sergej@bk.ru>
2024-09-06 15:48:09 +0200

    dt

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
 import sys
+
+pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
+
 
 def main ():
     print("Starting asteroids!")
-  
-    while True:
+    running = True
+    
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+                running = False
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+
 
 if __name__ == "__main__":
     main()
