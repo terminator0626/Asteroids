9dbb53fc30f6970fcb595a83776da31faa2b3c3a
terminator0626 <terminator.sergej@bk.ru>
2024-09-09 16:48:59 +0200

    correct

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import pygame
 from constants import *
-import sys
-from circleshape import *
-from player import *
+from player import Player
 
-pygame.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-dt = 0
-x = SCREEN_WIDTH / 2
-y = SCREEN_HEIGHT / 2
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-def main ():
-
-    player = Player(x, y)
-    print("Starting asteroids!")
-    running = True
-    
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                running = False
-        
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
+                return
+
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
